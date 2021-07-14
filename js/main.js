fe5310f9be8862bbfa175570693c72f04c202219
ActBoy_G <uran252525@gmail.com>
2021-07-14 22:14:43 +0900

    水アニメーション　制作

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-$(function(){
-    
-    window.onload = function(){
-
-        let totals = $(".card").data();
-        console.log(totals)
+$(function () {
+  var waves = function () {
+    for (let i = 0; i < 8; i++) {
+      var totals = [];
+      var card = $(".card").eq(i);
+      totals[i] = card.data();
+      var cardWaves = [];
+      cardWaves[i] = $(`.card span`).eq(i).attr("class");
+      $("." + cardWaves[i]).css("top", totals[i].total);
     }
+  };
+  waves();
+
+  
 });
